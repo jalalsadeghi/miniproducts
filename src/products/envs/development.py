b69e33df37b61553ec6d502ea3ebfb693d00b478
jalalsadeghi <jalal.a.sadeghi@gmail.com>
2023-11-19 20:48:58 +0330

    Debug srntry and sys

--- src/products/envs/development.py
+++ src/products/envs/development.py
@@ -1,1 +1,23 @@
 from .common import *
+import sentry_sdk
+import sys
+
+DEBUG=True
+
+sentry_sdk.init(
+    dsn="https://4d0a878ab23ada2c40af8f20731befcb@o4506253563330560.ingest.sentry.io/4506253569032192",
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    traces_sample_rate=1.0,
+    # Set profiles_sample_rate to 1.0 to profile 100%
+    # of sampled transactions.
+    # We recommend adjusting this value in production.
+    profiles_sample_rate=1.0,
+)
+
+
+def handler(t, i, s):
+    print("uncaught")
+
+
+sys.excepthook = handler
--- src/products/urls.py
+++ src/products/urls.py
@@ -16,6 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+
+def trigger_error(request):
+    raise Exception("another error")
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('sentry-debug/', trigger_error),
 ]
